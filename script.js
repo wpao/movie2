1f975dc8b19fa38dea2fa3e13d563ec8538f2947
wpao <wpaozan@gmail.com>
2023-12-31 13:59:46 +0800

    JAVASCRIPT LANJUTAN | 15. Error Handling

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 const searchButton = document.querySelector('.search-button')
 searchButton.addEventListener('click', async function(){
-    const inputKeyword = document.querySelector('.input-keyword')
-    const movies = await getMovies(inputKeyword.value)
-    updateUI(movies)
+    try {
+        const inputKeyword = document.querySelector('.input-keyword')
+        const movies = await getMovies(inputKeyword.value)
+        updateUI(movies)
+    } catch (err) {
+        // console.log(err)
+        alert(err)
+    }
+    
 })
+
+function getMovies(keyword) {
+    return fetch('http://omdbapi.com/?apikey=529b2788&s=' + keyword)
+        .then(response => { 
+            if(!response.ok){
+                if(response.statusText === '') {
+                    throw new Error(response.statusText = 'Unauthorized')
+                }
+                throw new Error(response.statusText)
+            }
+            return response.json()
+        })
+        .then(response => { 
+            if (response.Response === "False") {
+                throw new Error(response.Error)
+            }
+            return response.Search
+        })
+}
+
+function updateUI(movies) {
+    let cards = ''
+    movies.forEach( m => cards += showCards(m))
+    const movieContainer = document.querySelector('.movie-container')
+    movieContainer.innerHTML = cards
+}
 
 // event binding
 document.addEventListener('click', async function(e){
@@ -24,20 +56,6 @@ function updateUIDetail(m){
     const movieDetail = showMovieDetail(m)
     const modalBody = document.querySelector('.modal-body')
     modalBody.innerHTML = movieDetail
-}
-// 
-
-function getMovies(keyword) {
-    return fetch('http://omdbapi.com/?apikey=529b2788&s=' + keyword)
-        .then(response => response.json())
-        .then(response => response.Search)
-}
-
-function updateUI(movies) {
-    let cards = ''
-    movies.forEach( m => cards += showCards(m))
-    const movieContainer = document.querySelector('.movie-container')
-    movieContainer.innerHTML = cards
 }
 
 function showCards(m){
